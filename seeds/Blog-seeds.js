91494a375c25b270218d1897c52c9449d2f4446b
Anu.Saini <105421965+Anu-Saini@users.noreply.github.com>
2022-09-15 00:22:22 +1000

    committing

--- seeds/Blog-seeds.js
+++ seeds/Blog-seeds.js
@@ -11,18 +11,18 @@ Blogger_id: 1,
   title:'Authenticaton vs. Authorization',
   content: 'There is a difference between austhentication and authorization. Authentication means confirming your own identity,where as authorization means being allowed to access the system',
   blog_dt: '03/09/2022' ,
-  user_id: 2,
+  Blogger_id: 2,
 }, {
   title:'test1',
   content: 'test1test1test1test1test1test1test1test1test1test1test1test1test1',
   blog_dt: '04/09/2022' ,
-  user_id: 2,
+  Blogger_id: 2,
 },
 {
   title:'test 000022220000222',
   content: 'test 000022220000222test 000022220000222test 000022220000222test 000022220000222test 000022220000222test 000022220000222',
   blog_dt: '05/09/2022' ,
-  user_id: 3,
+  Blogger_id: 3,
 },
 
 ];

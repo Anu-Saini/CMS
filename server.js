800b0be4c6481e8b9ddacf96a92d059026a17c85
Anu.Saini <105421965+Anu-Saini@users.noreply.github.com>
2022-09-14 22:02:36 +1000

    seeds

--- server.js
+++ server.js
@@ -47,6 +47,6 @@ app.use(express.static(path.join(__dirname, 'public/')));
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
-seedAll();
+// seedAll();
   app.listen(PORT, () => console.log(`Now listening at localhost:, ${ PORT }`));
 });
